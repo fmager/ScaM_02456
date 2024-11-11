4df4b40f228653c2d0ab7e382c11f8bf6d746711
fmager <fmager@dtu.dk>
2024-11-11 16:01:25 +0200

    Remove .vscode and start_pytorch.sh from tracking

--- start_pytorch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-module load LUMI/24.03 partition/G PrgEnv-amd PyTorch/2.2.0-rocm-5.6.1-python-3.10-singularity-20240315
-wd=/users/magerfab/PhD/ijepa
-singularity exec -B $wd:/workdir $SIF /bin/bash  -c "export PYTHONPATH=\"${PYTHONPATH}:/users/magerfab/PhD/ScaM\"; exec /bin/bash"
